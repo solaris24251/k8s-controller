a32958bff3d2df1ef0e14410d02f44b07c88eefa
AntonS <solaris24@gmail.com>
2025-06-24 09:26:26 +0000

    step4-fasthttp-server

---
+++ cmd/server.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+	"github.com/valyala/fasthttp"
+)
+
+var serverPort int
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Start a FastHTTP server",
+	Run: func(cmd *cobra.Command, args []string) {
+		handler := func(ctx *fasthttp.RequestCtx) {
+			fmt.Fprintf(ctx, "Hello from FastHTTP!")
+		}
+		addr := fmt.Sprintf(":%d", serverPort)
+		log.Info().Msgf("Starting FastHTTP server on %s", addr)
+		if err := fasthttp.ListenAndServe(addr, handler); err != nil {
+			log.Error().Err(err).Msg("Error starting FastHTTP server")
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+	serverCmd.Flags().IntVar(&serverPort, "port", 8080, "Port to run the server on")
+}
---
+++ cmd/server_test.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestServerCommandDefined(t *testing.T) {
+	if serverCmd == nil {
+		t.Fatal("serverCmd should be defined")
+	}
+	if serverCmd.Use != "server" {
+		t.Errorf("expected command use 'server', got %s", serverCmd.Use)
+	}
+	portFlag := serverCmd.Flags().Lookup("port")
+	if portFlag == nil {
+		t.Error("expected 'port' flag to be defined")
+	}
+}
